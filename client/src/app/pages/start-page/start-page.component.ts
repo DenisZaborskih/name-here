8076910e85e4ebe2b64061d9dfaf0f62162130f4
DenisZaborskih <dzabors@mail.ru>
2025-02-21 13:31:58 +0700

    wip: начата верстка стартовой страницы, добавлены ассеты

--- client/src/app/pages/start-page/start-page.component.ts
+++ client/src/app/pages/start-page/start-page.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-start-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.scss'
 })
 export class StartPageComponent {
-
+  public categories : number[] = [1,2,3,4,5,6,7,8,9,10];
 }
