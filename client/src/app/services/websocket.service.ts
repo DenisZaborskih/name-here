8f982ea7e18d7ceef5076a9803eb511c80e1bf2a
DenisZaborskih <dzabors@mail.ru>
2025-03-09 22:12:31 +0700

    wip: частично готов сервис для вебсокета

--- client/src/app/services/websocket.service.ts
+++ client/src/app/services/websocket.service.ts
@@ -1,5 +1,6 @@
 
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 export interface Message {
   content: string;
@@ -9,5 +10,33 @@ export interface Message {
 
 @Injectable({ providedIn: 'root' })
 export class WebSocketService {
-  
+  private ws!: WebSocket;
+  private messageSubject = new BehaviorSubject<string | null>(null);
+
+  public initWebSocket() {
+    this.ws.onopen = () => {
+      console.log(`WebSocket init success`);
+    }
+
+    this.ws.onmessage = (event) => {
+      this.messageSubject.next(event.data);
+    }
+
+    this.ws.onerror = (error) => {
+      console.error(`WebSocket was lost with error : ${error}`)
+    }
+  }
+
+  public sendMessage(msg: string) {
+    if (this.ws.readyState === WebSocket.OPEN && this.ws) {
+      this.ws.send(msg);
+    }
+    else {
+      console.error(`WebSocket error while send msg/ robably not open`);
+    }
+  }
+
+  public closeWebSocket() {
+    if (this.ws) this.ws.close;
+  }
 }
